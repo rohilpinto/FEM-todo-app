62f08721e2a8df1568599e387b37610af6d094a1
rohilpinto <rohilcrispinto@gmail.com>
2021-02-13 19:49:48 +0530

    did js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-const addBtn = document.querySelector(".todo--btn");
+const markAll = document.querySelector(".todo--btn");
 
 const todoTextContainer = document.querySelector(".todo--text-container");
 
 let todoInput = document.querySelector(".todo--input");
 
+const clearAllBtn = document.querySelector(".todo--btn__clear-all");
+
+const todoRemaining = document.querySelector(".todo--remaining");
+
+// -------------------------------------------------------------------
+
 todoInput.addEventListener("keydown", function (e) {
   let keycode = 13;
-  if (e.keyCode == keycode) {
+  if (e.keyCode == keycode && todoInput.value !== "") {
     createElement();
   }
 });
@@ -16,22 +22,54 @@ function createElement() {
   let newBtnElement = document.createElement("button");
   let p = document.createElement("span");
   let div = document.createElement("div");
+  let delbtn = document.createElement("button");
+
+  // console.log(childNodes + 1)
 
   todoTextContainer.appendChild(div);
-  newBtnElement.innerText = "✕";
+  newBtnElement.innerText = "X";
   todoTextContainer.appendChild(p);
   p.innerText = inputValue;
+  delbtn.innerText = "X";
 
   div.appendChild(newBtnElement);
   div.appendChild(p);
+  div.appendChild(delbtn);
+
+  todoInput.value = "";
+
+  delbtn.classList.add("newBtnElement");
 
   newBtnElement.classList.add("newBtnElement");
+
+  // adds completed class to the p variable ( span element in html)
   newBtnElement.addEventListener("click", function () {
     p.classList.toggle("completed");
   });
-	
+
+  // clears all todos with compeleted if completetd css class is assigned
+  clearAllBtn.addEventListener("click", function () {
+    if (p.className === "completed") {
+      p.parentElement.remove();
+    }
+  });
+
+  // marks all todos as complete
+  markAll.addEventListener("click", function () {
+    p.classList.add("completed");
+  });
+
+  // removes parent div thus removing a the enitre todo
+  delbtn.addEventListener("click", function (e) {
+    this.parentElement.remove();
+  });
+
+  let childNodes = todoTextContainer.childNodes.length;
+
+  for (let i = 0; i < childNodes; i++) {
+    if(childNodes < i) {
+      i--;
+   }
+    todoRemaining.innerText = i;
+  }
 }
-
-
-
- 
