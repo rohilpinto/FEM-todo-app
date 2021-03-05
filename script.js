f5e7fb9af35a4d337a72e6fdf216b1e8c3676180
rohilpinto <rohilcrispinto@gmail.com>
2021-03-05 12:47:10 +0530

    added sun-moon theme-icons

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ toggleBtn.onclick = () => {
 if(body.className.includes('light')) {
 	body.classList.remove('light')
 	body.classList.add('dark')
-	toggleBtn.innerText = "Light"
+	// toggleBtn.innerText = "Light"
 	 
 } else {
 	
 	body.classList.remove('dark')
 
 	body.classList.add('light')
-	toggleBtn.innerText = "Dark"
+	// toggleBtn.innerText = "Dark"
 }
 
 };
